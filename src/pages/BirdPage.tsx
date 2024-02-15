6c7c4db0845faf14e51cf2884f04252b78daa0fd
kencakes <vansaet.kendra@gmail.com>
2024-02-15 11:48:27 +0100

    Read and delete operation for bird cards

--- src/pages/BirdPage.tsx
+++ src/pages/BirdPage.tsx
@@ -1,9 +1,15 @@
+import Card from "@/components/Card";
 import PageTitle from "@/components/PageTitle";
+import { BirdContent, BirdContentProvider } from "@/context/BirdProvider";
 
 const BirdPage = () => {
     return (
         <div className="flex flex-col gap-4 w-full">
+
             <PageTitle title="Birds" />
+            <BirdContentProvider>
+                <Card />
+            </BirdContentProvider>
         </div>
     )
 }
